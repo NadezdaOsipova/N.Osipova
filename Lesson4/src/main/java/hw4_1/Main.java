68d81f81cb06a54a8a764cefd999eb956b67fbe1
NadezdaOsipova <kaliadina@mail.ru>
2021-08-28 15:49:18 +0300

    Lesson4

--- Lesson4/src/main/java/hw4_1/Main.java
+++ Lesson4/src/main/java/hw4_1/Main.java
@@ -4,14 +4,14 @@ package hw4_1;
 public class Main {
 
     public static void main(String[] args) {
-       // Person person = new Person();
+        // Person person = new Person();
 
-       Person person = new Person("Иван","Иванов",12,"Samsung");
+        Person person = new Person("Иван", "Иванов", 12, "Samsung");
         MetodPerson metodPerson = new MetodPersonImpl(person);
-     //   Person person2 = new Person("Петр","Петров",21,"LG");
+        //   Person person2 = new Person("Петр","Петров",21,"LG");
         //  Person person3 = new Person("Федор","Федоров",18,"Sony");
-metodPerson.printAllInformation();
-metodPerson.printName();
-metodPerson.isAdult();//почему не выводится true|false
+        metodPerson.printAllInformation();
+        metodPerson.printName();
+        System.out.println(metodPerson.isAdult());
     }
 }
