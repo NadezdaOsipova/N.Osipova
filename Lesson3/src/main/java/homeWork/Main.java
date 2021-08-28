9d05f2916287d8c541115b016bbedfb9b22329ce
NadezdaOsipova <kaliadina@mail.ru>
2021-08-28 17:34:22 +0300

    Lesson3

--- Lesson3/src/main/java/homeWork/Main.java
+++ Lesson3/src/main/java/homeWork/Main.java
@@ -1,15 +1,40 @@
 package homeWork;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        if ()
+        Random random = new Random();
 
-        int a = scanner.nextInt();
+        System.out.println("Введите число:");
+        int a = 0;
+        if (scanner.hasNextInt()) {
+            a = scanner.nextInt();
+        } else {
+            System.out.println(a + "- Вы ввели некорректное число!");
+        }
 
+        int[] myArray = new int[a];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = (int) (Math.random() * 10);
+            System.out.print(myArray[i] + " ");
+        }
+        System.out.println();
 
-        int [] myArray = new
+        Massive massive = new Massive();
+        System.out.println("_____");
+        massive.printMassiveAsLine(myArray);
+        System.out.println();
+        massive.printReverseMassiveAsLine(myArray);
+        System.out.println();
+        System.out.println("_____");
+
+        System.out.println(massive.getSumOfElements(myArray));
+        System.out.println("_____");
+
+        System.out.println(massive.multiplyBy3(myArray));
+        System.out.println("_____");
     }
 }
