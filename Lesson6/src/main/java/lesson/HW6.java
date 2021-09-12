fbde1daac8d83eea52874df1edd2b50284be1f80
Nadezda Osipova <kaliadina@mail.ru>
2021-09-12 13:26:14 +0300

    Lesson 6.1

---
+++ Lesson6/src/main/java/lesson/HW6.java
@@ -0,0 +1,68 @@
+package lesson;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*1. Ввести 3 строки с консоли, найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.
+  2. Ввести 3 строки с консоли. Упорядочить и вывести строки в порядке возрастания значений их длины. (Посмотреть способы сортировки)
+  3. Ввести 3 строки с консоли. Вывести на консоль те строки, длина которых меньше средней, а также их длину.
+  4. Ввести 3 строки с консоли. Найти слово, состоящее только из различных символов. Если таких слов несколько, найти первое из них.
+  5. Ввести 3 строки с консоли. Среди слов, состоящих только из цифр, найти слово-палиндром. Если таких слов больше одного, найти второе из них.
+*/
+public class HW6 {
+    public static void main(String[] args) {
+        //  System.out.println("Введите 3 строки:");
+        Scanner scanner = new Scanner(System.in);
+        //String line = scanner.nextLine();
+        //String line1 = scanner.nextLine();
+        //String line2 = scanner.nextLine();
+
+        //String[] lins = {line, line1, line2};
+
+        // System.out.println("1. Ввести 3 строки с консоли, найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.");
+        // String minValue = lins[0];
+        // String maxValue = lins[0];
+
+        // for (int i = 0; i < lins.length; i++) {
+        //     if (lins[i].length() >= maxValue.length()) {
+        //         maxValue = lins[i];
+        //     }
+        //     if (lins[i].length() <= minValue.length()) {
+        //         minValue = lins[i];
+        //     }
+        // }
+        // System.out.println(maxValue);
+        // System.out.println(minValue);
+
+        //  System.out.println("2. Упорядочить и вывести строки в порядке возрастания значений их длины. (Посмотреть способы сортировки)");
+
+//        Arrays.sort(lins);
+        //     for (int i = 0; i < lins.length; i++) {
+        //        System.out.print(lins[i]+ " ");
+        //}
+
+
+        // System.out.println("3. Вывести на консоль те строки, длина которых меньше средней, а также их длину.");
+
+
+        // System.out.println("4. Найти слово, состоящее только из различных символов. Если таких слов несколько, найти первое из них.");
+
+
+        // System.out.println("5. Среди слов, состоящих только из цифр, найти слово-палиндром. Если таких слов больше одного, найти" +
+        //                  " второе из них.\n");
+        // System.out.println("Введите 3 строки, состоящие из цифр:");
+        // String number1 = String.valueOf(scanner.nextInt());
+        // String number2 = String.valueOf(scanner.nextInt());
+        // String number3 = String.valueOf(scanner.nextInt());
+
+        // String[] numbers = {number1, number2, number2};
+
+        // for (int i = 0; i < numbers.length; i++) {
+        //     String reverseString = new StringBuilder(numbers[i]).reverse().toString();
+        //     if (reverseString.equalsIgnoreCase(numbers[i])) {
+        //         System.out.println(numbers[i] + "- слово-палиндром");
+        //     }
+        // }
+
+    }
+}
