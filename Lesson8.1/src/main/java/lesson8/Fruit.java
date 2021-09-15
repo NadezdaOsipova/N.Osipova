bf8996e6f7c12f3a9e0d941c030cd88a53bba7fc
NadezdaOsipova <kaliadina@mail.ru>
2021-09-15 20:15:41 +0300

    Lesson 8

--- Lesson8.1/src/main/java/lesson8/Fruit.java
+++ Lesson8.1/src/main/java/lesson8/Fruit.java
@@ -1,17 +1,13 @@
 package lesson8;
 
 public class Fruit {
-    public Pips pips;
 
-    public Fruit() {
-        this.pips = new Pips();
-    }
 
-    class Pips {
+    static class Pips {
         static final int count = 4;
 
-        //  public int getCount() {
-        //      return count;
-        //  }
+        public static int getCount() {
+            return count;
+        }
     }
 }
--- Lesson8.1/src/main/java/lesson8/Main1.java
+++ Lesson8.1/src/main/java/lesson8/Main1.java
@@ -4,7 +4,8 @@ import lesson8.Fruit;
 
 public class Main1 {
     public static void main(String[] args) {
+        Fruit.Pips.getCount();
 
-        System.out.println(Fruit.Pips.count);
+        //   System.out.println(Fruit.Pips.count);
     }
 }
