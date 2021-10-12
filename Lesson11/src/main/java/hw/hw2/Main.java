f4375b9167a0a8f0e81412c90414b3cd781d8e2c
NadezdaOsipova <kaliadina@mail.ru>
2021-10-13 01:13:04 +0300

    Lesson 11

--- Lesson11/src/main/java/hw/hw2/Main.java
+++ Lesson11/src/main/java/hw/hw2/Main.java
@@ -1,18 +1,18 @@
 package hw.hw2;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         AnimalList list = new AnimalList();
-        //   AnimalList animalList = new AnimalList();
-        list.addAnimal(Monkey);
-        animalList.addAnimal("Tiger");
-        animalList.addAnimal("Monkey");
-        animalList.addAnimal("Elephant");
-        animalList.addAnimal("Leon");
-        System.out.println(animalList);
-        animalList.removeAnimal();
-        animalList.removeAnimal();
-        System.out.println(animalList.toString());
+        list.addAnimal(new Monkey());
+        list.addAnimal(new Elephant());
+        list.addAnimal(new Wolf());
+
+        System.out.println(list);
+        list.removeAnimal();
+        list.removeAnimal();
+        System.out.println(list.toString());
     }
 }
